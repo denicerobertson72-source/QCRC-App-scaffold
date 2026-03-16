b474ae4d32c70367777f7ce55cca720e1cbe1ecb
denicerobertson72-source <denicerobertson72@gmail.com>
2026-03-16 11:18:00 -0400

    Use exact QCRC logo asset and replace numeric skill levels in UI

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -7,7 +7,7 @@ export function TopNav() {
     <header className="topnav">
       <nav>
         <div className="topnav-home">
-          <img src="/qcrc-lockup.svg" alt="QCRC" className="topnav-logo topnav-logo-plain" />
+          <img src="/QCRC.png" alt="QCRC" className="topnav-logo topnav-logo-plain" />
           <Link href="/reservations">Reservations</Link>
         </div>
         <Link href="/reserve">Reserve</Link>
